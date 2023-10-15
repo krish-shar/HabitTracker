01ba72fddb8964aa20f63f21d9157006bada5d26
nikitagni <nagnihotri6@gatech.edu>
2023-10-15 04:48:42 -0400

    mindfulness restructured

--- client/components/MindfulSection.tsx
+++ client/components/MindfulSection.tsx
@@ -4,7 +4,7 @@ import { CircularProgress, CircularProgressLabel, Box, Flex, Button } from '@cha
 const MindfulSection = () => {
   const [countdown, setCountdown] = useState(300); // Initial countdown value
   const [timerStarted, setTimerStarted] = useState(false);
-  const videoURL = '/purple-live-wallpaper.mp4'; // Path to your video in the public directory
+  const videoURL = '/please_work.mp4'; // Path to your video in the public directory
 
   useEffect(() => {
     if (timerStarted) {
@@ -47,7 +47,7 @@ const MindfulSection = () => {
               <CircularProgress
                 value={(countdown / 10) * 100} // Adjust value for the circular timer
                 size="200px"
-                thickness="12px"
+                thickness="10px"
                 color="green.500"
               >
                 <CircularProgressLabel>{`${countdown}s`}</CircularProgressLabel>
@@ -66,12 +66,12 @@ const MindfulSection = () => {
             <CircularProgress
               value={100}
               size="200px"
-              thickness="12px"
+              thickness="10px"
               color="green.500"
             >
               <CircularProgressLabel>0s</CircularProgressLabel>
             </CircularProgress>
-            <Button onClick={handleRestartTimer} size="lg" colorScheme="red" mt={4}>
+            <Button onClick={handleRestartTimer} size="md" colorScheme="light red" mt={4} justifyItems={'center'}>
               Restart
             </Button>
           </div>
